4cc97aedf11d7c7c767f21c16ed9e905b928efc5
Angel Igareta <angel@igareta.com>
2019-12-15 12:42:39 +0100

    Add crossvalidation for hyperparameter tuning.

--- src/main/scala/mlmodels/DecisionTreeModel.scala
+++ src/main/scala/mlmodels/DecisionTreeModel.scala
@@ -1,10 +1,9 @@
 package mlmodels
 
 import org.apache.spark.ml.Pipeline
-import org.apache.spark.ml.regression.{
-  DecisionTreeRegressionModel,
-  DecisionTreeRegressor
-}
+import org.apache.spark.ml.evaluation.RegressionEvaluator
+import org.apache.spark.ml.regression.DecisionTreeRegressor
+import org.apache.spark.ml.tuning.{CrossValidator, ParamGridBuilder}
 import org.apache.spark.sql.DataFrame
 import preprocess.PreProcessDataset
 import tuning.HyperparameterTuning
@@ -20,25 +19,38 @@ object DecisionTreeModel {
       .setLabelCol("ArrDelay")
       .setFeaturesCol("features")
 
+    // Pipeline to prepare the data before training the model
     val pipeline = new Pipeline()
       .setStages(pipelineStages ++ Array(dt))
 
-    // Train model. This also runs the indexer.
-    val model = pipeline.fit(trainingData)
+    // Evaluator we want to use to choose the best model.
+    val evaluator = new RegressionEvaluator()
+      .setLabelCol("ArrDelay")
+      .setPredictionCol("prediction")
+      .setMetricName("rmse")
 
-    // Save model
-    model.write
-      .overwrite()
-      .save(Utils.MODEL_PATH + "/decision_tree")
+    // We use a ParamGridBuilder to construct a grid of parameters to search over.
+    val paramGrid = new ParamGridBuilder()
+      .addGrid(dt.maxDepth, Array(5, 7, 9, 11))
+      .build()
+
+    // Cross Validator will contribute to a better hyperparameter tuning
+    val cv = new CrossValidator()
+      .setEstimator(pipeline)
+      .setEvaluator(evaluator)
+      .setEstimatorParamMaps(paramGrid)
+      .setNumFolds(5)
+
+    // Train model using Cross Validator.
+    val model = cv.fit(trainingData)
 
     // Make predictions.
     val predictions = model.transform(testData)
     HyperparameterTuning.showModelPrecision(predictions);
 
-    // Show DecisionTree
-    val treeModel: DecisionTreeRegressionModel = model
-      .stages(pipelineStages.length)
-      .asInstanceOf[DecisionTreeRegressionModel]
-    println("Learned regression tree model:\n" + treeModel.toDebugString)
+    // Save model
+    model.write
+      .overwrite()
+      .save(Utils.MODEL_PATH + "/decision_tree")
   }
 }
--- src/main/scala/mlmodels/RandomForestModel.scala
+++ src/main/scala/mlmodels/RandomForestModel.scala
@@ -1,10 +1,9 @@
 package mlmodels
 
 import org.apache.spark.ml.Pipeline
-import org.apache.spark.ml.regression.{
-  RandomForestRegressionModel,
-  RandomForestRegressor
-}
+import org.apache.spark.ml.evaluation.RegressionEvaluator
+import org.apache.spark.ml.regression.RandomForestRegressor
+import org.apache.spark.ml.tuning.{CrossValidator, ParamGridBuilder}
 import org.apache.spark.sql.DataFrame
 import preprocess.PreProcessDataset
 import tuning.HyperparameterTuning
@@ -20,24 +19,39 @@ object RandomForestModel {
       .setLabelCol("ArrDelay")
       .setFeaturesCol("features")
 
+    // Pipeline to prepare the data before training the model
     val pipeline = new Pipeline()
       .setStages(pipelineStages ++ Array(rf))
 
-    // Train model. This also runs the indexer.
-    val model = pipeline.fit(trainingData)
+    // Evaluator we want to use to choose the best model.
+    val evaluator = new RegressionEvaluator()
+      .setLabelCol("ArrDelay")
+      .setPredictionCol("prediction")
+      .setMetricName("rmse")
 
-    // Save model
-    model.write
-      .overwrite()
-      .save(Utils.MODEL_PATH + "/random_forest")
+    // We use a ParamGridBuilder to construct a grid of parameters to search over.
+    val paramGrid = new ParamGridBuilder()
+      .addGrid(rf.numTrees, Array(10, 100, 200))
+      .addGrid(rf.maxDepth, Array(10, 30, 50))
+      .build()
+
+    // Cross Validator will contribute to a better hyperparameter tuning
+    val cv = new CrossValidator()
+      .setEstimator(pipeline)
+      .setEvaluator(evaluator)
+      .setEstimatorParamMaps(paramGrid)
+      .setNumFolds(5)
+
+    // Train model using Cross Validator.
+    val model = cv.fit(trainingData)
 
     // Make predictions.
     val predictions = model.transform(testData)
     HyperparameterTuning.showModelPrecision(predictions);
 
-    val rfModel = model
-      .stages(pipelineStages.length)
-      .asInstanceOf[RandomForestRegressionModel]
-    println(s"Learned regression forest model:\n ${rfModel.toDebugString}")
+    // Save model
+    model.write
+      .overwrite()
+      .save(Utils.MODEL_PATH + "/random_forest")
   }
 }
